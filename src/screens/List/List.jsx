a200363b99ce3420f1da052488601cebba32740b
Michał Nowak <nowakmichal9696@gmail.com>
2021-06-07 16:49:44 +0200

    Added possibility to remove and update task status

--- src/screens/List/List.jsx
+++ src/screens/List/List.jsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Input, Button, CheckBox } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { ApiService } from '../../services/Api.service';
@@ -8,53 +8,62 @@ import { ApiService } from '../../services/Api.service';
 export const ListScreen = () => {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
-  const [finishedTasks, setFinishedTasks] = useState(0);
 
   const fetchData = async () => {
     const fetchedTasks = await ApiService.GetTasks();
     setTasks(fetchedTasks);
   };
 
-  const onAddNewTask = () => {
-    if (!newTask) return null;
-    setTasks([onCreateTask(newTask), ...tasks]);
-    setNewTask('');
+  const onCreateTask = async () => {
+    const isAddedSuccess = await ApiService.CreateTask(newTask);
+    if (isAddedSuccess) {
+      setNewTask('');
+      fetchData();
+    }
   };
 
-  const onCreateTask = (description) => {
-    const id = new Date().getUTCMilliseconds();
-    const task = {
-      id,
-      description,
-      completed: false,
-    };
-    return task;
-  };
-
-  const onHandleCheck = (id) => {
+  const onHandleCheck = async (id) => {
     const allTasks = [...tasks];
     const item = allTasks.find((item) => item.id === id);
     item.completed = !item.completed;
     setTasks([...allTasks]);
+    const response = await ApiService.UpdateTask(item);
+    if (!response) {
+      fetchData();
+    }
   };
 
-  const onUpdateFinishedTasks = () => {
-    const numberOfFinishedTasks = tasks.filter((i) => i.completed).length;
-    setFinishedTasks(numberOfFinishedTasks);
+  const onRemoveFinishedTasks = async () => {
+    const response = await ApiService.BulkDelete();
+    if (response) {
+      fetchData();
+    }
   };
-
-  const onRemoveFinishedTasks = () => {
-    const uncompletedTasks = tasks.filter((t) => !t.completed);
-    setTasks(uncompletedTasks);
-  };
-
-  useEffect(() => {
-    onUpdateFinishedTasks();
-  }, [tasks]);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const flatListItemRenderer = ({ item }) => {
+    const handleCheckItem = () => {
+      onHandleCheck(item.id);
+    };
+    return (
+      <CheckBox
+        key={item.id}
+        checked={item.completed}
+        checkedColor="#0F0"
+        checkedTitle={item.description}
+        containerStyle={{ width: '95%' }}
+        onPress={handleCheckItem}
+        size={30}
+        textStyle={{}}
+        title={item.description}
+        titleProps={{}}
+        uncheckedColor="#F00"
+      />
+    );
+  };
 
   return (
     <View style={styles.root}>
@@ -62,45 +71,30 @@ export const ListScreen = () => {
         <Input
           placeholder="Add new task"
           rightIcon={
-            <Icon name="check" size={20} onPress={onAddNewTask} color="green" />
+            <Icon name="check" size={20} onPress={onCreateTask} color="green" />
           }
           onChangeText={setNewTask}
           value={newTask}
         />
       </View>
       <View style={styles.list}>
-        {tasks.map(({ description, id, completed }) => {
-          const handleCheckItem = () => {
-            onHandleCheck(id);
-          };
-          return (
-            <CheckBox
-              key={id}
-              checked={completed}
-              checkedColor="#0F0"
-              checkedTitle={description}
-              containerStyle={{ width: '95%' }}
-              onPress={handleCheckItem}
-              size={30}
-              textStyle={{}}
-              title={description}
-              titleProps={{}}
-              uncheckedColor="#F00"
-            />
-          );
-        })}
+        <FlatList
+          data={tasks}
+          renderItem={flatListItemRenderer}
+          keyExtractor={(item) => item.id}
+        />
       </View>
       <View style={styles.removeChecked}>
         <Button
           buttonStyle={{ minWidth: '80%' }}
           containerStyle={{ margin: 20 }}
-          icon={<Icon name="trash" size={15} color="#0FF" />}
+          icon={<Icon name="trash" size={15} color="white" />}
           loadingProps={{ animating: true }}
           onPress={onRemoveFinishedTasks}
           title="Remove finished items"
           titleProps={{}}
           titleStyle={{ marginHorizontal: 20 }}
-          disabled={finishedTasks === 0}
+          disabled={tasks.filter((i) => i.completed).length === 0}
         />
       </View>
     </View>
--- src/services/Api.service.js
+++ src/services/Api.service.js
@@ -1,18 +1,56 @@
 import axios from 'axios';
 
+const uri = 'http://localhost:4000/tasks';
+
 export class ApiService {
   static async GetTasks() {
     let mappedData = [];
-    // const response = await axios.get(uri).catch((e) => console.error(e));
+    const response = await axios.get(uri).catch((e) => console.error(e));
 
-    // if (response && response.data && response.data?.success) {
-    //   const data = response.data.content;
-    //   mappedData = data.map(({ id, description, completed }) => ({
-    //     id,
-    //     description,
-    //     completed,
-    //   }));
-    // }
+    if (response && response.data) {
+      mappedData = response.data.map(({ _id, description, completed }) => ({
+        id: _id,
+        description,
+        completed,
+      }));
+    }
     return mappedData;
+  }
+
+  static async CreateTask(description) {
+    const response = await axios
+      .post(uri, {
+        description,
+        completed: false,
+      })
+      .catch((e) => console.error(e));
+
+    if (response && response.data) {
+      return true;
+    }
+    return false;
+  }
+
+  static async UpdateTask(task) {
+    const response = await axios
+      .put(`${uri}/${task.id}`, {
+        description: task.description,
+        completed: task.completed,
+      })
+      .catch((e) => console.error(e));
+
+    if (response && response.data) {
+      return true;
+    }
+    return false;
+  }
+
+  static async BulkDelete() {
+    const response = await axios.delete(uri).catch((e) => console.error(e));
+
+    if (response && response.data) {
+      return true;
+    }
+    return false;
   }
 }
